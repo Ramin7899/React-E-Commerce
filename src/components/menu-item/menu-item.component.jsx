31db6124ba25f3c5605d8433e5237e7fb54966bd
Ramin <raminahmadi7899@gmail.com>
2021-09-23 14:09:19 +0330

    update packages

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,13 +2,18 @@ import React from "react";
 
 import './menu-item.style.scss'
 
-const MenuItem = ({ title, imageurl,size }) => (
-    <div style={{
-        backgroundImage: `url(${imageurl})`
-    }} className={`${size} menu-item`}>
+const MenuItem = ({ title, imageurl, size }) => (
+    <div className={`${size} menu-item`} >
+
+        <div style={{
+            backgroundImage: `url(${imageurl})`
+        }} className="background-image">
+
+        </div>
+
         <div className="content">
             <h1 className="title">
-                {title}
+                {title.toUpperCase()}
             </h1>
             <span className="subtitle">
                 SHOP NOW
