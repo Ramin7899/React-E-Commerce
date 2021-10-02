d7f47e2d6b1472c2aee94dae07a3d98ba8e5d4fb
Ramin <raminahmadi7899@gmail.com>
2021-10-02 17:17:58 +0330

    shop page style

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router';
 import './App.css';
-import ShopPage from './pages/shop/shop.component'
+import ShopPage from './pages/shop/shop.component';
+import Header from "./components/header/header.component"
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
-
     </div>
   );
 }
